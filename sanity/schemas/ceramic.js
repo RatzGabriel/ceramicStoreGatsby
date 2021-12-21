9c76ecaa291f8ab5a03c9c1a449c9963b694e5d6
Gabriel <gabrielratz84@gmail.com>
2021-12-21 10:47:00 +0100

    Feat: ceramic list

--- sanity/schemas/ceramic.js
+++ sanity/schemas/ceramic.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import PriceInput from '../components/PriceInput';
 
 import { GiTeapotLeaves as icon } from 'react-icons/gi';
 export default {
   name: 'ceramic',
   title: 'Ceramic',
   type: 'document',
+
   icon,
   fields: [
     {
@@ -34,8 +36,33 @@ export default {
       name: 'price',
       title: 'Price',
       type: 'number',
+      inputComponent: PriceInput,
       description: 'Price of the item in cents',
       validation: (Rule) => Rule.min(1000).max(50000),
     },
+    {
+      name: 'eigenschaften',
+      title: 'Eigenschaften',
+      type: 'array',
+      of: [{ type: 'reference', to: [{ type: 'eigenschaften' }] }],
+    },
   ],
+  preview: {
+    select: {
+      title: 'name',
+      media: 'image',
+      eigenschaften0: 'eigenschaften.0.name',
+      eigenschaften1: 'eigenschaften.1.name',
+      eigenschaften2: 'eigenschaften.2.name',
+      eigenschaften3: 'eigenschaften.3.name',
+    },
+    prepare: ({ title, media, ...eigenschaften }) => {
+      const eig = Object.values(eigenschaften).filter(Boolean);
+      return {
+        title: title,
+        media,
+        subtitle: eig.join(', '),
+      };
+    },
+  },
 };
